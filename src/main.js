987e339005c651ccf70c02834ada03cf605fc77c
Pedro Alcantara Krivochein <pedroalcantarak@gmail.com>
2021-02-23 19:10:58 -0300

    Small changes

--- src/main.js
+++ src/main.js
@@ -178,7 +178,6 @@ $(".slides").sortable({
 });
 
 //sidenav source: https://www.w3schools.com/howto/howto_js_sidenav.asp
-
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
